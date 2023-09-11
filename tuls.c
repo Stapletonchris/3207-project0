efdda87700bbce9fb4d45863772d8f40f03a1774
Christopher Christmas <cchristmas21@c8-89-f3-c3-8c-0a.wireless.temple.edu>
2023-09-11 09:12:26 -0400

    Created the listFiles function so that it can run recursively

--- tuls.c
+++ tuls.c
@@ -1,42 +1,55 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <dirent.h>
 #include <sys/stat.h>
 
+void listFiles(const char *filename, DIR *directory);
+
 int main(int argc, char **argv)
 {
-    DIR *directory;
-    struct dirent *entry;
-
-    directory = opendir(".");
+    // Parse command line arguments
+    DIR *directory = opendir("filename");
 
     // Prints out the number of arguements passed to the program
     printf("Number of arguments: %d\n", argc);
 
+    listFiles(".", directory);
+
+    return 0;
+}
+
+void listFiles(const char *filename, DIR *directory)
+{
+    struct dirent *entry;
+
+    // Tests whether or not directory exists/can be opened
     if (directory == NULL)
     {
         perror("tuls: cannot open directory\n");
         exit(0);
     }
 
-    // While read through argv print the name of the argument
+    // Read through argc and argv
     while ((entry = readdir(directory)) != NULL)
     {
+        /*If its a regular file return the file name,
+        or if its a directory read through recursively */
         if (entry->d_type == DT_REG)
         {
             printf("File: %s\n", entry->d_name);
         }
-        else if (entry->d_type == DT_DIR)
+        else if (entry->d_type == DT_DIR && (strcmp(entry->d_name, ".") != 0) && (strcmp(entry->d_name, "..") != 0))
         {
+            listFiles(".", directory);
             printf("Dir: %s\n", entry->d_name);
         }
     }
-
+    // When argc == -1 close the directory
     if (closedir(directory) == -1)
     {
+        // If it failed to close print and error message
         perror("Error closing directory\n");
         exit(0);
     }
-
-    return 0;
 }
