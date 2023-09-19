c427050d479583411dea1d886803f09dcde97038
Christopher Christmas <cchristmas21@Christophers-MacBook-Pro.local>
2023-09-19 17:44:54 -0400

    Changed cpoyDir to copyToDir.

--- tucp.c
+++ tucp.c
@@ -5,7 +5,7 @@
 #include <sys/stat.h>
 #include <unistd.h>
 
-void copyDir(const char *dirName, const char *newDir);
+void copyToDir(const char *dirName, const char *newDir);
 void copyFile(const char *fileName, const char *newFile);
 
 int main(int argc, char **argv)
@@ -25,29 +25,24 @@ int main(int argc, char **argv)
 
     struct dirent *entry;
 
-    if (argc < 4 && entry->d_type == DT_REG)
-    {
-        // Copy argv[1] into the file at argv[2]
-        copyFile(argv[1], argv[2]);
-    }
+    const char *destination = argv[argc - 1];
 
-    // if (entry->d_type == DT_REG)
-    // {
-    //     // Command line error check
-    //     if (argc != 3)
-    //     {
-    //         perror("Invalid command line input; Argument number error.");
-    //         return 1;
-    //     }
-    //     else
-    //     {
-    //         copyFile(argv[1], argv[2]);
-    //     }
-    // }
-    // else if (entry->d_type == DT_DIR)
-    // {
-    //     copyDir(argv[1], argv[2]);
-    // }
+    for (int i = 0; i < argc; i++)
+    {
+        if (entry->d_type == DT_REG)
+        {
+            // Copy argv[1] into the file at argv[2]
+            copyFile(argv[i], argv[*destination]);
+        }
+        else if (entry->d_type == DT_DIR)
+        {
+            copyToDir(argv[i], argv[*destination]);
+        }
+        else
+        {
+            perror("I'm not sure what happend.")
+        }
+    }
 }
 
 void copyFile(const char *fileName, const char *fileCopy)
@@ -85,41 +80,38 @@ void copyFile(const char *fileName, const char *fileCopy)
     fclose(fileCopy);
 }
 
-// void copyDir(const char *dirName, const char *newDir)
-// {
-//     // Parse command line arguments
-//     DIR *directory = opendir(dirName);
-//     struct dirent *entry;
+void copyToDir(const char *dirName, const char *newDir)
+{
+    // Parse command line arguments
+    DIR *directory = opendir(dirName);
+    struct dirent *entry;
 
-//     // Tests whether or not directory exists/can be opened
-//     if (directory == NULL)
-//     {
-//         perror("tuls: cannot open directory\n");
-//         exit(0);
-//     }
+    // Tests whether or not directory exists/can be opened
+    if (directory == NULL)
+    {
+        perror("tuls: cannot open directory\n");
+        exit(0);
+    }
 
-//     // If the directory doesn't exist create one
-//     mkdir(newDir, 755);
+    // Read through argc and argv
+    while ((entry = readdir(directory)) != NULL)
+    {
+        /*If its a regular file return the file name,
+        or if its a directory read through recursively */
+        if (entry->d_type == DT_REG)
+        {
+            printf("File: %s\n", entry->d_name);
+        }
+        else if (entry->d_type == DT_DIR && (strcmp(entry->d_name, ".") != 0) && (strcmp(entry->d_name, "..") != 0))
+        {
+            // Specifiy the path to the directory
+            char path[100] = {0};
+            strcat(path, dirName);
+            strcat(path, "/");
+            strcat(path, entry->d_name);
 
-//     // Read through argc and argv
-//     while ((entry = readdir(directory)) != NULL)
-//     {
-//         /*If its a regular file return the file name,
-//         or if its a directory read through recursively */
-//         if (entry->d_type == DT_REG)
-//         {
-//             printf("File: %s\n", entry->d_name);
-//         }
-//         else if (entry->d_type == DT_DIR && (strcmp(entry->d_name, ".") != 0) && (strcmp(entry->d_name, "..") != 0))
-//         {
-//             // Specifiy the path to the directory
-//             char path[100] = {0};
-//             strcat(path, dirName);
-//             strcat(path, "/");
-//             strcat(path, entry->d_name);
-
-//             copyfile(path);
-//             printf("Dir: %s/%s\n", dirName, entry->d_name);
-//         }
-//     }
+            copyfile(path);
+            printf("Dir: %s/%s\n", dirName, entry->d_name);
+        }
+    }
 }
